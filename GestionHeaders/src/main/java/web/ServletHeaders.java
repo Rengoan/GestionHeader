ff30c0fbe0c8a3cc26d8cc78111747a59fc7b551
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-12-10 11:18:12 +0100

    12

--- GestionHeaders/src/main/java/web/ServletHeaders.java
+++ GestionHeaders/src/main/java/web/ServletHeaders.java
@@ -2,6 +2,7 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
@@ -19,7 +20,30 @@ public class ServletHeaders extends HttpServlet {
         
         out.print("<html>");
         out.print("<head>");
-        out.print("<html>");
+        out.print("<tittle>Header HTTP</tittle>");
+        out.print("</header>");
+        out.print("<body>");
+        out.print("<h1>Headers HTTP</h1>");
+        out.print("<br>");
+        out.print("Metodo HTTP: "+metodoHttp);
+        out.print("<br>");
+        
+        String uri = request.getRequestURI();
+        out.print("URI solicitada: "+uri);
+        out.print("<br>");
+        
+        Enumeration headers = request.getHeaderNames();
+        
+        while(headers.hasMoreElements()){
+            String nombreHeader = (String) headers.nextElement();
+            out.print("<b>" + nombreHeader + "</b");
+            out.print(request.getHeader(nombreHeader));
+            out.print("<br>");
+        }
+        
+        out.print("</body>");
+        out.print("</html>");
+        out.close();
         
     }
     
